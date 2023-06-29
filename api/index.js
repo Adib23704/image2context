4968e5c101f47086785eba4936838c09bacb6ec3
Adib23704 <adib23704@gmail.com>
2023-06-29 20:37:08 +0600

    Updated

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ const fastify = Fastify({
     logger: true
 })
 await fastify.register(import('fastify-raw-body'), {
-    field: 'rawBody', // change the default request.rawBody property name
-    global: false, // add the rawBody to every request. **Default true**
-    encoding: 'utf8', // set it to false to set rawBody as a Buffer **Default utf8**
-    runFirst: true, // get the body before any preParsing hook change/uncompress it. **Default false**
-    routes: [] // array of routes, **`global`** will be ignored, wildcard routes not supported
+    field: 'rawBody',
+    global: false,
+    encoding: 'utf8',
+    runFirst: true,
+    routes: []
 })
-fastify.register(import('./routes/main.js'));
+fastify.register(import('../src/main.js'));
 
 const port = 80;
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { httpClient } from '../config/config.js';
 dotenv.config();
 
-export default async function api(fastify, _options, done) {
+async function api(fastify, _options, done) {
     fastify.get('/', (_req, reply) => {
         reply.code(200).send({ status: true, message: 'Server is running...' });
     });
@@ -40,15 +40,13 @@ export default async function api(fastify, _options, done) {
     done();
 }
 
-function getKey()
-{
+function getKey() {
     let key = "";
-    switch(getRandomInt(1, 4))
-    {
-        case 1: key = process.env.KEY1; 
-        case 2: key = process.env.KEY2; 
-        case 3: key = process.env.KEY3; 
-        case 4: key = process.env.KEY4; 
+    switch (getRandomInt(1, 4)) {
+        case 1: key = process.env.KEY1;
+        case 2: key = process.env.KEY2;
+        case 3: key = process.env.KEY3;
+        case 4: key = process.env.KEY4;
     }
     return key;
 }
@@ -57,4 +55,6 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
+
+export default routes;
